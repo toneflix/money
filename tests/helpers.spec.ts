a0e186b5ed49205455fa81ff103506102fc544f1
3m1n3nc3 <mygames.ng@gmail.com>
2026-01-29 08:50:24 +0100

    test: refactor loadEnv test to improve environment variable checks

--- tests/helpers.spec.ts
+++ tests/helpers.spec.ts
@@ -7,9 +7,11 @@ describe('Helpers', () => {
     describe('loadEnv', () => {
         it('should load environment variables from .env file', async () => {
             const spy = vi.spyOn(helpers, 'useSetEnvLoaded').mockReturnValue()
+            const hasKey = Boolean(process.env.EXCHANGERATE_API_KEY)
 
-            if (!process.env.EXCHANGERATE_API_KEY) {
-                await loadEnv('.env.example')
+            await loadEnv('.env.example')
+
+            if (!hasKey) {
                 expect(process.env.EXCHANGERATE_API_KEY).toBe('0xxxxxxxxxx')
                 expect(spy).toHaveBeenCalledTimes(2)
             } else {
